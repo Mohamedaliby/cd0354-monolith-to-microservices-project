234f69970b0eae64ad4ac92121d5797101fc9964
Mohamed <abedaliby@yahoo.com>
2022-07-11 14:19:46 +0200

    updates apiHost in frontend

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,8 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'a66f6059b62e54d85aac09fbdb22dc06-503718348.us-east-1.elb.amazonaws.com:8080/api/v0'
+  // apiHost: 'http://localhost:8080/api/v0'
 };
 
 /*
